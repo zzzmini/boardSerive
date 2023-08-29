83fa37ccad8a610e9468f76fe441d6b117206ab8
zzz Kim <sm01079200887@gmail.com>
2023-08-29 10:42:58 +0900

    #9-Oauth2 설정파일
    1. application.yml 에 kakao 설정 추가
    2. SecurityConfig 파일에 OAuth2 로그인 설정 추가

--- examboard/src/main/java/com/example/examboard/config/SecurityConfig.java
+++ examboard/src/main/java/com/example/examboard/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.examboard.config;
 
+import com.example.examboard.service.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -14,6 +16,8 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    @Autowired
+    PrincipalOauth2UserService principalOauth2UserService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
@@ -31,6 +35,13 @@ public class SecurityConfig {
                 .logout(out->out
                         .logoutSuccessUrl("/articles/lists")
                         .logoutUrl("/logout"))
+
+                .oauth2Login(oAuth -> oAuth
+                        .loginPage("/user/login")
+                        .defaultSuccessUrl("/articles/lists")
+                        .userInfoEndpoint(userInfo-> userInfo
+                                .userService(principalOauth2UserService)))
+
 
                 .csrf(csrf -> csrf.disable());
         return http.build();
